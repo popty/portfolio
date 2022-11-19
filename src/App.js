feb253e44d582403d9d1211195481575d1063fbf
Akshay Thakur <akshay.thakur@olx.com>
2022-11-19 21:48:30 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import pages from './pages/pages';
+import components from './components/components';
+import '../src/styles/Navbar.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <components.Navbar/>
+        <Routes>
+          <Route path="/" element={<pages.Home/>} />
+          <Route path="/projects" element={<pages.Projects/>} />
+          <Route path="/project/:id" element = {<pages.ProjectDisplay/>} />
+          <Route path="/experience" element={<pages.Experience/>} />
+        </Routes>
+        <components.Footer/>
+      </Router>
     </div>
   );
 }
